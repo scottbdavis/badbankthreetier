4e2a1f8620a40908410e55686d1d0b6f4b7c89fc
Scott B. Davis <sdavisb@gmail.com>
2022-04-19 22:57:54 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+var express = require("express");
+var app = express();
+var cors = require("cors");
+var dal = require("./dal.js");
+
+app.use(express.static("public"));
+app.use(cors());
+
+//create user account
+
+app.get("/account/create/:name/:email/:password", function (req, res) {
+	//else create user
+	dal
+		.create(req.params.name, req.params.email, req.params.password)
+		.then((user) => {
+			console.log(user);
+			res.send(user);
+		});
+});
+
+//login user
+
+app.get("/account/all", function (req, res) {
+	dal.all().then((docs) => {
+		console.log(docs);
+		res.send(docs);
+	});
+});
+
+var port = 3000;
+app.listen(port);
+console.log("Running on port: " + port);
